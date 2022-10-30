25b1cf0d2b05d24b6fb47dd263666987b8360c01
unknown <nazar.bylen.ir.2021@lpnu.ua>
2022-10-30 13:23:19 +0200

    go right bottom-up func implemented

--- lab4/main.py
+++ lab4/main.py
@@ -11,12 +11,23 @@ characters = []
 for i in range(len(matrix)):
     line = f.readline()
     for char in line:
-        print(char)
         characters.append(char)
     for j in range(len(matrix)):
         matrix[i][j] = characters[j]
 
     characters.clear()
 
+
+def findPath(n, m):
+
+    if (m == 1 or n == 1):
+        return 1
+    return findPath(m, n - 1)
+
+
+
 if __name__ == '__main__':
     print(matrix)
+    n = nodes_first
+    m = nodes_second
+    print(findPath(n, m))
