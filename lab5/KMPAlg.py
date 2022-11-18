f73ce84a2ce554b8bf8d5aafd4fdfa4a15164f1a
unknown <nazar.bylen.ir.2021@lpnu.ua>
2022-11-18 17:39:55 +0200

    naming refactored, code cleaned

--- lab5/KMPAlg.py
+++ lab5/KMPAlg.py
@@ -2,34 +2,41 @@ import numpy as np
 
 
 def knuth_morris_pratt(string, substring):
-    def build_sub(string):
+    def build_sub(substring_to_match):
         j = 0
         i = 1
 
-        substring = np.full((len(string)), -1)
+        pattern__of_prefixes = np.full((len(substring_to_match)), -1)
 
-        while i < len(string):
-            if string[i] == string[j]:
-                substring[i] = j
+        while i < len(substring_to_match):
+
+            if substring_to_match[i] == substring_to_match[j]:
+                pattern__of_prefixes[i] = j
                 i, j = i + 1, j + 1
+
             elif j > 0:
-                j = substring[j - 1] + 1
+                j = pattern__of_prefixes[j - 1] + 1
+
             else:
                 i += 1
-        print(substring)
-        return substring
+        print(pattern__of_prefixes)
+        return pattern__of_prefixes
 
     def matcher(string, substring, pattern):
         i, j = 0, 0
         while i < len(string):
+
             if string[i] == substring[j]:
                 if j == len(substring) - 1:
                     return True
                 i, j = i + 1, j + 1
+
             elif j > 0:
                 j = pattern[j - 1] + 1
+
             else:
                 i += 1
+
         return False
 
     pattern = build_sub(substring)
--- lab5/morrison_tests.py
+++ lab5/morrison_tests.py
@@ -5,53 +5,53 @@ import KMPAlg
 def test_slow():
     string = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
     for i in range(15):
-        string = string + string
+        string += string
 
-    string = string + 'jab'
+    string += 'jab'
     substring = 'ajab'
 
     start = time.time()
-    assert KMPAlg.knuth_morris_pratt(string, substring) == True
+    assert KMPAlg.knuth_morris_pratt(string, substring) is True
 
     print("it took", time.time() - start, "seconds.")
 
-def test_mid_speed_fisrt():
+def test_middle_speed_fisrt():
     string = 'akajbakaklaklakajbakaklaklakajbakakla'
     for i in range(15):
-        string = string + string
+        string += string
 
-    string = string + 'akajbakaklaklj'
+    string += 'akajbakaklaklj'
 
     substring = 'akajbakaklaklj'
 
     start = time.time()
-    assert KMPAlg.knuth_morris_pratt(string, substring) == True
+    assert KMPAlg.knuth_morris_pratt(string, substring) is True
 
     print("it took", time.time() - start, "seconds.")
 
-def test_mid_speed_second():
+def test_middle_speed_second():
     string = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
     for i in range(15):
-        string = string + string
+        string += string
         if i == 8:
-            string = string + 'c'
+            string += 'c'
 
     substring = 'aaaac'
 
     start = time.time()
-    assert KMPAlg.knuth_morris_pratt(string, substring) == True
+    assert KMPAlg.knuth_morris_pratt(string, substring) is True
 
     print("it took", time.time() - start, "seconds.")
 
 def test_fast():
     string = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
     for i in range(15):
-        string = string + string
+        string += string
 
-    string = string + 'a'
+    string += 'a'
     substring = 'aaaaa'
 
     start = time.time()
-    assert KMPAlg.knuth_morris_pratt(string, substring) == True
+    assert KMPAlg.knuth_morris_pratt(string, substring) is True
 
     print("it took", time.time() - start, "seconds.")
